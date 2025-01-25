59ddbfb9f6375b6d91cb0bebaf9ad3ffee514c76
thetodd77 <ethanhpfox@gmail.com>
2025-01-25 11:08:32 -0500

    reading and printing .txt file

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+def main():
+    with open("/home/thetodd/workspace/github.com/EHPF-TheTodd/bookbot/books/frankenstein.txt") as f:
+        file_contents = f.read()
+    print(file_contents)
+
+main()
