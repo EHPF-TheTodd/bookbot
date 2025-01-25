41da7e03e2cd703ba38721b0762aaabce7dc4f83
thetodd77 <ethanhpfox@gmail.com>
2025-01-25 11:53:00 -0500

    add character count function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     file_contents = read_book_text("/home/thetodd/workspace/github.com/EHPF-TheTodd/bookbot/books/frankenstein.txt")
     print(file_contents)
     print(f"Word Count: {count_words(file_contents)}")
+    print(f"Character Count:\n{count_characters(file_contents)}")
 
 def read_book_text(path):
     with open(path) as f:
@@ -10,5 +11,14 @@ def read_book_text(path):
 def count_words(read_contents):
     words = read_contents.split()
     return len(words)
+
+def count_characters(read_contents):
+    char_count = {}
+    for character in read_contents.lower():
+        if character in char_count:
+            char_count[character] += 1
+        else:
+            char_count[character] = 1
+    return char_count
 
 main()
