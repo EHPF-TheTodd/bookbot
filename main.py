17f9e565159384d1aea4f71ffba192934b720db2
thetodd77 <ethanhpfox@gmail.com>
2025-01-25 11:14:01 -0500

    add a count words function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+def count_words(read_contents):
+    words = read_contents.split()
+    total = 0
+    for word in words:
+        total += 1
+    return total
+
 def main():
     with open("/home/thetodd/workspace/github.com/EHPF-TheTodd/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    print(f"Word Count: {count_words(file_contents)}")
 
 main()
